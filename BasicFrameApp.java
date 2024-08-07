8b789096a385b54e797b43bb48fa166f32adcf87
SteveInTech <stevemashia4@gmail.com>
2024-08-07 19:34:01 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,15 @@
+
+package basicframeapp;
+import za.ac.tut.ui.MyFirstFrame;
+
+public class BasicFrameApp {
+
+    public static void main(String[] args) {
+       
+       MyFirstFrame  myFrame;
+       
+       //instantiate the frame
+       myFrame = new MyFirstFrame();
+    }
+    
+}
